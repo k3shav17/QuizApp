3e0b168c4868dde8131198109100b0d3bad069ea
k3shav17 <keshavrao0489@gmail.com>
2022-08-11 18:14:37 +0530

    adding scores and sorting based on high score in Loc storage

--- end.js
+++ end.js
@@ -1,8 +1,11 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
-
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const MAX_HIGH = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -10,6 +13,17 @@ username.addEventListener('keyup', () => {
 });
 
 saveHighScore = e => {
-  console.log('clicked');
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => {
+    return b.score - a.score;
+  });
+  highScores.splice(5);
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('/');
 };
