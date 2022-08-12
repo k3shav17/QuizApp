d1249dc9c7984d18ef90c57dde11bff2fc8286a9
k3shav17 <keshavrao0489@gmail.com>
2022-08-12 12:04:53 +0530

    spinning loader is added

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnsers = false;
@@ -36,6 +38,7 @@ fetch('https://opentdb.com/api.php?amount=10&category=9&type=multiple')
 
       return formattedQuestion;
     });
+
     startGame();
   })
   .catch(err => console.log(err));
@@ -49,6 +52,9 @@ startGame = () => {
   score = 0;
   availableQuestions = [...questions];
   getNewQuestion();
+
+  game.classList.remove('hidden');
+  loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
